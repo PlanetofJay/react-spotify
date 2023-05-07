de352fe8f38f1d7e242b7cbd5dd29bd41d0c6b21
PlanetofJay <jae44dv@gmail.com>
2023-05-07 19:10:55 -0400

    finished Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,7 +12,7 @@ const NavLinks = ({ handleClick }) => (
       <NavLink
         key={item.name}
         to={item.to}
-        className='flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-200 hover:text-gray-400'
+        className='flex flex-row justify-start items-center my-8 text-sm font-medium text-black hover:text-gray-400'
         onClick={() => handleClick && handleClick()}
       >
         <item.icon className='w-6 h-6 mr-2' />
@@ -28,15 +28,36 @@ const Sidebar = () => {
 
   return (
     <>
-      <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483d8b] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
-        <img src={logo} alt='logo' className='w-full h-24 object-contain' />
-        <NavLinks handleClick={() => setMobileMenuOpen} />
+      <div className='md:flex hidden flex-col w-[240px] py-10 px-4'>
+        <img 
+          src={logo} 
+          alt='logo' 
+          className='w-full h-24 object-contain' 
+        />
+        <NavLinks />
       </div>
+
       <div className='absolute md:hidden block top-6 right-3'>
         {mobileMenuOpen
-          ? (<RiCloseLine className='w-6 h-6 text-white mr-2' />)
-          : <HiOutlineMenu className='w-6 h-6 text-white mr-2' />
+          ? (<RiCloseLine 
+              className='w-6 h-6 text-white mr-2' 
+              onClick={() => setMobileMenuOpen(false)}
+            />)
+          : <HiOutlineMenu 
+              className='w-6 h-6 text-white mr-2' 
+              onClick={() => setMobileMenuOpen(true)}
+            />
         }
+      </div>
+
+      <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from white/10 to-[#483d8b] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition
+      ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
+        <img 
+          src={logo} 
+          alt='logo' 
+          className='w-full h-24 object-contain' 
+        />
+        <NavLinks handleClick={() => setMobileMenuOpen(false)} />
       </div>
     </>
   );
