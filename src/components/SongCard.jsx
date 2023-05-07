e155c87fed4cffb7ce6050725ac5a3443f84656e
PlanetofJay <jae44dv@gmail.com>
2023-05-07 14:39:57 -0400

    update SongCard

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -6,13 +6,16 @@ import { playPause, setActiveSong } from "../redux/features/playerSlice";
 
 const SongCard = ({ song, isPlaying, activeSong, index, data }) => {
 
-  const handlePauseClick = () => {
+  const dispatch = useDispatch();
 
-  }
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
 
   const handlePlayClick = () => {
-
-  }
+    dispatch(setActiveSong({ song, data, index }));
+    dispatch(playPause(true));
+  };
 
   return (
     // [square brackets] with specific pixel
