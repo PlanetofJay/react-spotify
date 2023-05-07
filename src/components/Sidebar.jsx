ad66f22e4d3f83e77c9d83749859212adc116243
PlanetofJay <jae44dv@gmail.com>
2023-05-07 17:49:58 -0400

    update dynamic styles, Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { HiOutlineMenu } from 'react-icons/hi';
 import { RiCloseLine } from 'react-icons/ri';
 
 import { logo } from '../assets';
@@ -8,7 +9,7 @@ import { links } from '../assets/constants';
 const NavLinks = ({ handleClick }) => (
   <div className='mt-10'>
     {links.map((item) => (
-      <NavLink 
+      <NavLink
         key={item.name}
         to={item.to}
         className='flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-200 hover:text-gray-400'
@@ -26,12 +27,19 @@ const Sidebar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <div className='md:flex hidden flex-col w-[240px] py-10 px-4'>
-      <img src={logo} alt='logo' className='w-full h-24 object-contain' />
-      <NavLinks />
-    </div>
-  )
-
+    <>
+      <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483d8b] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
+        <img src={logo} alt='logo' className='w-full h-24 object-contain' />
+        <NavLinks handleClick={() => setMobileMenuOpen} />
+      </div>
+      <div className='absolute md:hidden block top-6 right-3'>
+        {mobileMenuOpen
+          ? (<RiCloseLine className='w-6 h-6 text-white mr-2' />)
+          : <HiOutlineMenu className='w-6 h-6 text-white mr-2' />
+        }
+      </div>
+    </>
+  );
 };
 
 export default Sidebar;
