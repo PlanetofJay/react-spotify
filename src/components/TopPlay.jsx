5abe0d62a706bd9e59cfff1059a076b18b449659
PlanetofJay <jae44dv@gmail.com>
2023-05-07 22:50:26 -0400

    update TopCharts section

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -14,7 +14,28 @@ import 'swiper/css/free-mode';
 
 const TopChartCard = ({ song, index }) => (
   <div className='w-full flex flex-row items-center hover:bg-gray-400/50 py-2 p-4 rounded-lg cursor-pointer mb-2'>
-    {song.title}
+    <h3 className='font-bold test-base text-white mr-3'>
+      {index + 1}.
+    </h3>
+    <div className='flex-1 flex flex-row justify-between items-center'>
+      <img 
+        src={song?.images?.coverart} 
+        alt={song?.title} 
+        className='w-20 h-20 rounded-lg'
+      />
+      <div className='flex-1 flex flex-col justify-center mx-3'>
+        <Link to={`/songs/${song.key}`}>
+          <p className='text-xl font-bold text-white'>
+            {song?.title}
+          </p>
+        </Link>
+        <Link to={`/artists/${song?.artists[0].adamid}`}>
+          <p className='text-base text-gray-300 mt-1'>
+            {song?.subtitle}
+          </p>
+        </Link>
+      </div>
+    </div>
   </div>
 )
 
