3394428813cce2e126d88288f3353f7324f648bc
PlanetofJay <jae44dv@gmail.com>
2023-05-08 15:02:26 -0400

    test commit after merge

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -1,5 +1,16 @@
-const DetailsHeader = () => (
-  <div>DetailsHeader</div>
-);
+import { Link } from 'react-router-dom';
+
+const DetailsHeader = ({ artistId, artistData, songData }) => {
+
+  return (
+    <div className='relative w-full flex flex-col'>
+      <div className='w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28' />
+    
+      <div>
+        
+      </div>
+    </div>
+  );
+};
 
 export default DetailsHeader;
