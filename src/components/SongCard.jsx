9ae0e143c3e7be6fd18d562396333ff030fb9dca
PlanetofJay <jae44dv@gmail.com>
2023-05-07 23:32:06 -0400

    update

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -19,7 +19,7 @@ const SongCard = ({ song, isPlaying, activeSong, index, data }) => {
 
   return (
     // [square brackets] with specific pixel
-    <div className='flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer'>
+    <div className='flex flex-col w-[250px] p-4 bg-white/5 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer'>
       <div className='relative w-full h-56 group'>
         <div className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex
         ${activeSong?.title === song.title
