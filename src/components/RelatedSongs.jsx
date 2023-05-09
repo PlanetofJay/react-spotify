4ab88fcadff5a311fe5613d839c2a0da5dd5e9a9
PlanetofJay <jae44dv@gmail.com>
2023-05-09 16:37:12 -0400

    fix bugs, update RelatedSongs

--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -1,5 +1,24 @@
-const RelatedSongs = () => (
-  <div>Loader</div>
+import Songbar from './SongBar';
+
+const RelatedSongs = ({ data, isPlaying, activeSong, handlePauseClick, handlePlayclick, artistId }) => (
+  <div className='flex flex-col'>
+    <h1 className='font-bold text-3xl text-white'>Related Songs:</h1>
+
+    <div className='mt-6 w-full flex flex-col'>
+      {data?.map((song, index) => (
+        <Songbar
+          key={`${artistId}-${song.key}-${index}`}
+          song={song}
+          index={index}
+          artistId={artistId}
+          isPlaying={isPlaying}
+          activeSong={activeSong}
+          handlePauseClick={handlePauseClick}
+          handlePlayClick={handlePlayclick}
+        />
+      ))}
+    </div>
+  </div>
 );
 
-export default RelatedSongs;
+export default RelatedSongs ;
