55f03ff511585dd4c3cb1d849fb04d7742e855f5
PlanetofJay <jae44dv@gmail.com>
2023-05-10 00:57:12 -0400

    finished Searchbar section

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -2,32 +2,45 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FiSearch } from 'react-icons/fi';
 
-const Searchbar = () => (
-  <form 
-    autoComplete='off'
-    className='p-2 text-gray-400 focus-within:text-gray-600'
-  >
-    <label
-      htmlFor='search-field'
-      className='sr-only'  
+const Searchbar = () => {
+
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    navigate(`/search/${searchTerm}`);
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      autoComplete='off'
+      className='p-2 text-gray-400 focus-within:text-gray-600'
     >
-     Search all songs 
-    </label>
-    <div className='flex flex-row justify-start items-center'>
-      <FiSearch className='w-5 h-5 ml-4 text-white' />
-      <input
-        name='search-field'
-        autoComplete='off'
-        id='search-field'
-        placeholder='Search'
-        type='search'
-        value=''
-        // TODO: improve this
-        onChange={() => {}}
-        className='flex-1 bg-transparent border-none outline-none placeholder-white text-base text-white p-4'
-      />
-    </div>
-  </form>
-);
+      <label
+        htmlFor='search-field'
+        className='sr-only'
+      >
+        Search all songs
+      </label>
+
+      <div className='flex flex-row justify-start items-center'>
+        <FiSearch className='w-5 h-5 ml-4 text-white' />
+        <input
+          name='search-field'
+          autoComplete='off'
+          id='search-field'
+          placeholder='Search'
+          type='search'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className='flex-1 bg-transparent border-none outline-none placeholder-white text-base text-white p-4'
+        />
+      </div>
+    </form>
+  );
+};
 
 export default Searchbar;
