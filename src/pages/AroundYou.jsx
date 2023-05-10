cac5a0c3ae5432f97e87784c52a7ca154c438b75
PlanetofJay <jae44dv@gmail.com>
2023-05-09 23:07:45 -0400

    finished AroundYou section

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -23,10 +23,25 @@ const AroundYou = () => {
   if (isFetching && loading) return <Loader title='Loading songs around you' />;
 
   if (error && country) return <Error />;
-  
+
   return (
     <div className='flex flex-col'>
+      <h2 className='font-bold text-3xl text-white text-left mt-4 mb-10'>
+        Around You, <span className='font-black'>{country}</span>
+      </h2>
 
+      <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
+        {data?.map((song, index) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+            index={index}
+          />
+        ))}
+      </div>
     </div>
   );
 };
